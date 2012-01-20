20d24e07064419742f0b1689aa35add0e8d269c2
Ben Firshman <ben@firshman.co.uk>
2012-01-20 15:59:34 +0000

    Moar data!

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
 from flask import Flask, jsonify, request
 import socket
 
+socket.setdefaulttimeout(5)
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    ip = request.remote_addr
-    ipv4 = request.remote_addr.rsplit(':', 1)[-1]
-    return jsonify(
-        ip=ip,
-        ipv4=ipv4,
-        host=socket.gethostbyaddr(ipv4)[0]
-    )
+    d = {
+        'ipv6': None,
+        'hostv6': None,
+        'ipv4': None,
+        'hostv4': None,
+    }
+    d['ipv4'] = request.remote_addr.rsplit(':', 1)[-1]
+    try:
+        d['hostv4'] = socket.gethostbyaddr(d['ipv4'])[0]
+    except: # oh so many things can go wrong
+        pass
+    if ':' in request.remote_addr:
+        d['ipv6'] = request.remote_addr
+        try:
+            d['hostv6'] = socket.gethostbyaddr(d['ipv6'])[0],
+        except:
+            pass
+    return jsonify(**d)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
 
 
